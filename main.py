f124d48771425df951fa0812c054b7e06160083c
Michael Brown <michlfbrown@gmail.com>
2023-11-12 16:24:17 -0800

    added pydantic. get list of top 100 unique streams

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from typing import List
 from dotenv import load_dotenv
 import os
 import httpx
 from authlib.integrations.httpx_client import OAuth2Client
+from pydantic import BaseModel
 
 load_dotenv()
 token_endpoint = "https://id.twitch.tv/oauth2/token"
+
+
+class Stream(BaseModel):
+    user_id: str
+    user_name: str
+    game_id: str
+    game_name: str
+    language: str
+    viewer_count: int
 
 
 def get_streams(
@@ -32,8 +43,32 @@ client = OAuth2Client(
 client.fetch_token(token_endpoint)
 
 # Get top 100 streams
-response = get_streams(client, language="en", limit=100)
-streams = response.json()["data"]
+streams = []
+language = "en"
+response = get_streams(client, language=language, limit=100)
+data = response.json()["data"]
+
+for stream in data:
+    streams.append(Stream(**stream))
+
+# get next page because previous response does not always include 100 streams
+cursor = response.json()["pagination"]["cursor"]
+response = get_streams(client, language=language, limit=20, after=cursor)
+data2 = response.json()["data"]
+for stream in data2:
+    streams.append(Stream(**stream))
+
+# get final list of 100 streams
+seen_ids = set()
+unique_streams = []
+for stream in streams:
+    id = stream.user_id
+
+    if id not in seen_ids:
+        unique_streams.append(stream)
+        seen_ids.add(id)
+
+unique_streams = unique_streams[:100]
 
 # Merge with already tracked streams
 
